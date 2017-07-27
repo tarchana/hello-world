3064022dd51ac758089ae727d3aed0cf97410b1b
tarchana <archanacsit2007@gmail.com>
2017-07-28 00:05:57 +0530

    Update PrintRandom.java

--- PrintRandom.java
+++ PrintRandom.java
@@ -3,19 +3,6 @@ public class PrintRandom {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int count=1;
-for(int i=0;i<100;i++)
-	
-	{
-	
-	i=i+8;
-	System.out.println(i);
-	count++;
-	if(count>4)
-	{
-	 break;	
-	}
-	}
 
 	}
 
